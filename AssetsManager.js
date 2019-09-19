e302e4fcf4108bc2c6cefb40ed048c9c4041e615
Luiz Ferreira <luiz.ferreira@engenharia.ufjf.br>
2019-09-19 20:08:30 -0300

    add image via canvas

--- AssetsManager.js
+++ AssetsManager.js
@@ -1,4 +1,16 @@
 function AssetsManager(){
     this.aCarregar = 0;
     this.carregadas = 0;
+    this.assets = {};
+}
+AssetsManager.prototype.load = function(key, url){
+    console.log(`Carregando imagem ${url}...`)
+    this.aCarregar++;
+    var imagem = new image();
+    console.log(`Imagem ${key}: ${url} carregada.`);
+    imagem.scr = url;
+    this.assets[key] = imagem;
+    imagem.addEventListen("load",function(){
+        this.carregadas++;
+    });
 }
